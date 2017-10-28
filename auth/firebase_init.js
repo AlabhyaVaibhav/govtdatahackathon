c60286559b53bb8b36281107be8c74e7ba46e4af
AlabhyaVaibhav <alabhya.vaibhav1997@gmail.com>
2017-10-28 09:52:56 +0530

    auth & charts added]

---
+++ auth/firebase_init.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+//include <script src="https://www.gstatic.com/firebasejs/4.6.0/firebase.js"></script> in the main website
+var config = {
+  apiKey: "AIzaSyBUV_Zg6IdGdnlumoL1BhoNVgUrSOssyy0",
+  authDomain: "tars-cb0de.firebaseapp.com",
+  databaseURL: "https://tars-cb0de.firebaseio.com",
+  projectId: "tars-cb0de",
+  storageBucket: "",
+  messagingSenderId: "865186913885"
+  };
+  firebase.initializeApp(config);
